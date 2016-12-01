f59a5704682bf3d21241445de09412c711872745
Javier Sánchez Rois <javier.sanchez.rois@gmail.com>
2016-12-01 12:28:39 +0100

    Update commented_out_code.sh

--- commented_out_code.sh
+++ commented_out_code.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+total_lines=0
+readonly threshold=2
 if [ "$#" -ge 1 ]; then 
  first_commit=$1
 else
@@ -13,9 +14,16 @@ for commit in $(git log $first_commit..HEAD --pretty=format:"%h") ; do
   commented_out_lines=$(git show $commit --no-ext-diff --unified=0 --exit-code -a --no-prefix -- $file | egrep "^\+//")
   commented_out_lines_count=$(echo "$commented_out_lines" | wc -l | awk '{print $1}')
   if [ "$commented_out_lines_count" -gt 1 ]; then
+   total_lines=$(($total_lines+$commented_out_lines_count))
    echo "Found the following $commented_out_lines_count commented-out lines in file $file:"
    echo "$commented_out_lines"
   fi
  done
 done
+
+echo "Summary:"
+echo "Found a total of $total_lines commented-out lines of code."
  
+if [ "$total_lines" -ge $threshold ]; then
+ return 1
+fi

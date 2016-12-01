50cbacb88298cb80ebc86c67a51c8d16657156c7
Javier Sánchez Rois <javier.sanchez.rois@gmail.com>
2016-12-01 12:22:42 +0100

    Fixed some errors in commented-out code detector

--- commented_out_code.sh
+++ commented_out_code.sh
@@ -9,8 +9,8 @@ fi
 
 for commit in $(git log $first_commit..HEAD --pretty=format:"%h") ; do
  echo "Analyzing commit $commit ..."
- for file in $(git diff-tree --no-commit-id --name-only -r HEAD) ; do
-  commented_out_lines=$(git show $commit --no-ext-diff --unified=0 --exit-code -a --no-prefix $file | egrep "^\+//")
+ for file in $(git diff-tree --no-commit-id --name-only -r $commit) ; do
+  commented_out_lines=$(git show $commit --no-ext-diff --unified=0 --exit-code -a --no-prefix -- $file | egrep "^\+//")
   commented_out_lines_count=$(echo "$commented_out_lines" | wc -l | awk '{print $1}')
   if [ "$commented_out_lines_count" -gt 1 ]; then
    echo "Found the following $commented_out_lines_count commented-out lines in file $file:"
